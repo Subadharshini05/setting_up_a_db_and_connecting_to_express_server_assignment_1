f11a31e9f62bfbf2e93175c259b739ed07e9b86c
suba dharshini <subakarthick2005@gmail.com>
2025-02-08 10:08:53 +0530

    Completed

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
+require('dotenv').config(); 
+
 const express = require('express');
-const { resolve } = require('path');
-
 const app = express();
-const port = 3010;
+const port = process.env.PORT || 3010;
+const dotenv =require("dotenv");
+const connectDatabase = require('./database');
 
-app.use(express.static('static'));
-
-app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
+app.get('/ping', (req, res) => {
+    res.send('Pong!');
 });
 
+connectDatabase();
+
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`Server is running at http://localhost:${port}`);
 });
